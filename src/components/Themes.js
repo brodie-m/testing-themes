e0183338684ffb19d5ff8ae4c1e1034f5dfcc9f0
brodie-m <brodiedmcguire@gmail.com>
2021-10-13 16:55:05 +0100

    added themes component

---
+++ src/components/Themes.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+    body: `#CCC`,
+    text: `#363537`,
+    toggleBorder: `#CCC`,
+    background: `#363537`
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096'
+    background: '#999'
+
+}
