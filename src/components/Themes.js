c36994eb496f03482eb07d9abc069ab778b5e738
brodie-m <brodiedmcguire@gmail.com>
2021-10-13 16:55:23 +0100

    fix

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -7,7 +7,7 @@ export const lightTheme = {
 export const darkTheme = {
     body: '#363537',
     text: '#FAFAFA',
-    toggleBorder: '#6B8096'
+    toggleBorder: '#6B8096',
     background: '#999'
 
 }
