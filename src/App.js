7e6a3c17297f53e68cb80cdb00d02669d7f84517
brodie-m <brodiedmcguire@gmail.com>
2021-10-14 10:20:36 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import dummyData from "./data";
 //components
 import CustomNavbar from "./components/CustomNavbar";
 import CardList from "./components/CardList";
-
+import NotFound from './components/NotFound'
 //start theme stuff
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { lightTheme, darkTheme } from "./components/Themes";
 //end theme stuff
 
+//router
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
 //bootstrap
-import {Button} from 'react-bootstrap'
+import { Button } from "react-bootstrap";
 import TitleHighlight from "./components/TitleHighlight";
 
 const App = () => {
   const [videos, setVideos] = useState([]);
 
   //theme stuff
-  const [theme,setTheme]=useState('dark');
+  const [theme, setTheme] = useState("dark");
   function themeToggler() {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
+    theme === "light" ? setTheme("dark") : setTheme("light");
   }
-
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -35,34 +41,43 @@ const App = () => {
   }, []);
 
   return (
-      <ThemeProvider theme = {theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyles/>
-      <CustomNavbar/>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
         
-        <div className="App">     
+        <Route path="/home" exact>
           
-          {
-            videos.map((list, index) => {
-              return (
-                <section key={index}>
-                  <TitleHighlight title={list.section}>
-                 
-                  </TitleHighlight>
-                    
-                  
-                  
-                  <CardList list={list} />
-                  <hr />
-                </section>
-              );
-            })}
+        <CustomNavbar />
+
+        <div className="App">
+          {videos.map((list, index) => {
+            return (
+              <section key={index}>
+                <TitleHighlight title={list.section}></TitleHighlight>
+
+                <CardList list={list} />
+                <hr />
+              </section>
+            );
+          })}
         </div>
-        <footer className = 'd-flex justify-content-center'>
-        <Button onClick={themeToggler} className = 'btn btn-dark'>Switch theme</Button>
+        <footer className="d-flex justify-content-center">
+          <Button onClick={themeToggler} className="btn btn-dark">
+            Switch theme
+          </Button>
         </footer>
-        </ThemeProvider>
-      
-    
+          </Route>
+          <Route path="*">
+          <CustomNavbar />
+          <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
