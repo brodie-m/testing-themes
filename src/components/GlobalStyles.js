0eb36a041864f0cf345a5f2622e17d57c94d4a4b
brodie-m <brodiedmcguire@gmail.com>
2021-10-14 10:11:33 +0100

    styling changes

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -13,5 +13,35 @@ export const GlobalStyles = createGlobalStyle`
     }
     .navbar {
         background: ${({ theme }) => theme.navBody};
+        color: ${({ theme }) => theme.primary};
+    }
+    .nav-link {
+        color: ${({ theme }) => theme.primary};
+    }
+    .section-title {
+        
+        text-shadow: 3px 3px rgba(0,0,0,0.2);
+        color: ${({ theme }) => theme.primary}
+    }
+    .card-title {
+        text-shadow: 1px 1px rgba(0,0,0,0.2);
+        color: ${({ theme }) => theme.secondary }
+
+    }
+    .title-highlight {
+        border-radius: 0px 0px 10px 10px;
+        top: 50px;
+        margin-left: -150px;
+        z-index: -2;
+        background: ${({ theme }) => theme.titleHighlight};
+        width: 20vw;
+        height: 5vh;
+        overflow: none;
+    }
+    .title-highlight h2 {
+        top: -50px;
+        margin-left: 120px;
+        overflow: none;
+        white-space: nowrap;
     }
 `
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -6,15 +6,27 @@ export const lightTheme = {
     toggleBorder: `#CCC`,
     background: `#363537`,
     font: `arial`,
+    padding: `0.5rem`
     
 }
 export const darkTheme = {
+    primary: `#BB86FC`,
+    primaryVariant: `#3700B3`,
+    secondary: `#03DAC6`,
+    onPrimary : `#000000`,
+    onSecondary : `#000000`,
     body: '#363537',
+    titleHighlight: `#252426`,
     cardBody: `#474648`,
     navBody: '#474648',
     text: '#FAFAFA',
     toggleBorder: '#6B8096',
-    background: '#999',
+    background: '#121212',
     font: `arial`,
+    padding: `0.5rem`
     
+}
+
+export const eightiesTheme = {
+    // body:
 }
